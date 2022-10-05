2f18da80fe756a680025c8227ebb3f02f2d793f9
pablito <55856800+probablypablito@users.noreply.github.com>
2022-10-05 08:25:17 -0400

    idk man

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-CMD ["python3", "SaveTheWorldClaimer.py"]
+CMD ["/bin/bash", "start.sh"]
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+log=/proc/1/fd/1
+
+cd ./webserver python3 main.py > $log 2>&1
+cd ..
+
+python3 SaveTheWorldClaimer.py > $log 2>&1
