89b545132e67c3fecdca9bb1a840e17e9e8f1041
pablito <55856800+probablypablito@users.noreply.github.com>
2022-10-04 22:22:05 -0400

    Clean up some stuff

--- test.py
+++
@@ -1,5 +0,0 @@
-import requests, json
-
-url = "https://discord.com/api/webhooks/1027002584680116274/YbeUat0MJaoWVNWbZ4cbJDIVvoR9gGKrdlANQgcrRjvMa5CaNPiS3LyaeqvXEDeofUqo"
-data = {'content': f"The refresh token has expired." }
-requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
